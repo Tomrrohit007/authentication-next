67829102e50ecb32bef2726e526bad6ea6e7bded
Tomrrohit007 <tomarrohit5034@gmail.com>
2024-04-22 22:56:31 +0530

    oauth's added for both google and github

--- auth.ts
+++ auth.ts
@@ -1,9 +1,54 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+
 import authConfig from "./auth.config";
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
+import { UserRole } from "@prisma/client";
 
-export const { signIn, signOut, auth } = NextAuth({
+// extending the session type here
+declare module "next-auth" {
+  interface Session {
+    user: {
+      role: UserRole;
+    } & DefaultSession["user"];
+  }
+}
+
+export const { signIn, signOut, auth, handlers } = NextAuth({
+  callbacks: {
+    // these callbacks will run whenever the action happend like forexample signIn will run after when user signIn and execute the code inside it.
+    // async signIn({ user }) {
+    //   if (!user.id) {
+    //     return false;
+    //   }
+    //   const existingUser = await getUserById(user.id);
+    //
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+    //
+    //   return true;
+    // },
+    async session({ token, session }) {
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
