9c2224aeb2651cfdbcdc9fc91a27542ff910c483
Tomrrohit007 <tomarrohit5034@gmail.com>
2024-05-12 10:03:43 +0530

    fixed prisma error

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,36 +1,36 @@
-"use client";
+'use client';
 
-import { useRouter } from "next/navigation";
-import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
-import { LoginForm } from "./login-form";
+import { useRouter } from 'next/navigation';
+import { Dialog, DialogContent, DialogTrigger } from '../ui/dialog';
+import { LoginForm } from './login-form';
 
 type LoginButtonProps = {
   children: React.ReactNode;
-  mode?: "modal" | "redirect";
+  mode?: 'modal' | 'redirect';
   asChild?: boolean;
 };
 
 export const LoginButton = ({
   children,
-  mode = "redirect",
+  mode = 'modal',
   asChild,
 }: LoginButtonProps) => {
   const router = useRouter();
   const onClick = () => {
-    router.push("/auth/login");
+    router.push('/auth/login');
   };
-  if (mode === "modal") {
+  if (mode === 'modal') {
     return (
       <Dialog>
         <DialogTrigger asChild={asChild}>{children}</DialogTrigger>
-        <DialogContent className="p-0 w-auto bg-transparent border-none">
+        <DialogContent className='p-0 w-auto bg-transparent border-none'>
           <LoginForm />
         </DialogContent>
       </Dialog>
     );
   }
   return (
-    <span onClick={onClick} className="cursor-pointer">
+    <span onClick={onClick} className='cursor-pointer'>
       {children}
     </span>
   );
